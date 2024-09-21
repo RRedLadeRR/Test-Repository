3d4a142c752307cea6f50b4b974efbb7147f5644
RRedLadeRR <kuznetsovroman775@gmail.com>
2024-09-21 18:18:29 +0300

    deleted f1, f2, f3

--- f2.py
+++
@@ -1,6 +0,0 @@
-#Timofey
-product_name = input("Назва продукту ")
-quantity = int(input("Кількість продукту "))
-price = float(input("Ціна продукту "))
-total_cost = quantity * price
-print(f"Продукт: {product_name}, Кількість: {quantity}, Вартість: {total_cost}")
--- f3.py
+++
@@ -1,4 +0,0 @@
-#Timofey
-number = int(input("Введіть число "))
-for i in range(1, 11):
-    print(f"{number} x {i} = {number * i}")
